7dde41eb290d8c111912b7e7e14acec9062f7ddc
shivkara <109614368+shivkara@users.noreply.github.com>
2023-04-27 10:03:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.oneclick = () => {
+    menuIcon.classList.toggle('menuicon')
+    navbar.classList.toggle('active')
+}
+
+
+
+
+// scroll section acvtive link
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header navbar a');
 
@@ -11,14 +24,50 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navlinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header navbar a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
+
+
+    // sticky navbar
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+    // remove taggle icon and navbar link
+    menuIcon.classList.remove('fa-solid fa-x')
+    navbar.classList.remove('active')
+
 };
   
+
+// scroll reveal
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration:2000,
+    Delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:
+     'bottom' });
+     ScrollReveal().reveal('.home-content h1,.about-Img', { origin:'left' });
+     ScrollReveal().reveal('.home-content p,.about-content', { origin:'right' });
   
 
+// =============================typed js==========
+const typed = new Typed('#multiple-text', {
+    strings: ['Frontend Developer', 'youtuber', 'blogger'],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+// import Typed from 'typed.js';
+
+// const typed = new Typed('.multiple-text', {
+//   strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+//   typeSpeed: 50,
+// });
